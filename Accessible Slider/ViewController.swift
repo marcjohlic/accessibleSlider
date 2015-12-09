9d4ef7b0f00b54d3a672aa6d145115b5c3a907b8
Marc Johlic <marc.johlic@gmail.com>
2015-12-09 11:04:25 -0500

    Added slider and labels - plus code to increment slider.

--- Accessible Slider/ViewController.swift
+++ Accessible Slider/ViewController.swift
@@ -9,6 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var displayedValue: UILabel!
+    
+    @IBOutlet weak var slider: UISlider!
+
+    @IBAction func sliderValueChanged(sender: UISlider) {
+        
+        let currentValue = Int(sender.value)
+        
+        displayedValue.text = "\(currentValue)%"
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
